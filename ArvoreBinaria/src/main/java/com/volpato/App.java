9649833818d0a9b83978d8a1208f0dc1a9ab1824
Lara Volpato <lara.volpato@edu.pucrs.br>
2025-10-09 14:10:39 +0000

    Arvore Binaria 09/10

---
+++ ArvoreBinaria/src/main/java/com/volpato/App.java
@@ -0,0 +1,7 @@
+package com.volpato;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto ArvoreBinaria criado com sucesso.");
+    }
+}
